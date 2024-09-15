2ea942971402f470d87ea07df8ff2d9330583395
christopher-pedraza <62347713+christopher-pedraza@users.noreply.github.com>
2024-09-15 04:16:01 -0600

    Pruebas con ShadCN

--- dashboard/dashboard/src/BigComponents/SecondaryDashboard/SecondaryDashboard.tsx
+++ dashboard/dashboard/src/BigComponents/SecondaryDashboard/SecondaryDashboard.tsx
@@ -1,53 +1,95 @@
-import React from "react";
+// import React from "react";
 
-import {
-    Dialog,
-    DialogContent,
-    DialogDescription,
-    DialogHeader,
-    DialogTitle,
-    DialogTrigger,
-} from "@/components/ui/dialog";
+// import {
+//     Dialog,
+//     DialogContent,
+//     DialogDescription,
+//     DialogHeader,
+//     DialogTitle,
+//     DialogTrigger,
+// } from "@/components/ui/dialog";
 
-import {
-    Collapsible,
-    CollapsibleContent,
-    CollapsibleTrigger,
-} from "@/components/ui/collapsible";
+// import {
+//     Collapsible,
+//     CollapsibleContent,
+//     CollapsibleTrigger,
+// } from "@/components/ui/collapsible";
 
-import { Input } from "@/components/ui/input";
+// import { Input } from "@/components/ui/input";
 
-const SecondaryDashboard: React.FC = () => {
-    return (
-        <Dialog>
-            <DialogTrigger>+ Crear campaña</DialogTrigger>
-            <DialogContent>
-                <DialogHeader>
-                    <DialogTitle>Nueva campaña</DialogTitle>
-                    <DialogDescription>
-                        <Collapsible>
-                            <CollapsibleTrigger>
-                                Perfil demográfico
-                            </CollapsibleTrigger>
-                            <CollapsibleContent>
-                                Yes. Free to use for personal and commercial
-                                projects. No attribution required.
-                            </CollapsibleContent>
-                        </Collapsible>
-                        <Collapsible>
-                            <CollapsibleTrigger>
-                                Configuración de tono del bot
-                            </CollapsibleTrigger>
-                            <CollapsibleContent>
-                                Yes. Free to use for personal and commercial
-                                projects. No attribution required.
-                            </CollapsibleContent>
-                        </Collapsible>
-                    </DialogDescription>
-                </DialogHeader>
-            </DialogContent>
-        </Dialog>
-    );
-};
+// import {
+//     Form,
+//     FormControl,
+//     FormDescription,
+//     FormField,
+//     FormItem,
+//     FormLabel,
+//     FormMessage,
+// } from "@/components/ui/form";
 
-export default SecondaryDashboard;
+// import { Button } from "@/components/ui/button";
+
+// const SecondaryDashboard: React.FC = () => {
+//     function onSubmit(data: any) {
+//         console.log(data);
+//     }
+
+//     return (
+//         <Dialog>
+//             <DialogTrigger>+ Crear campaña</DialogTrigger>
+//             <DialogContent>
+//                 <DialogHeader>
+//                     <DialogTitle>Nueva campaña</DialogTitle>
+//                     <DialogDescription>
+//                         <Form
+//                             onSubmit={onSubmit}
+//                             className="w-2/3 space-y-6"
+//                         >
+//                             <FormField
+//                                 control={form.control}
+//                                 name="username"
+//                                 render={({ field }) => (
+//                                     <FormItem>
+//                                         <FormLabel>Username</FormLabel>
+//                                         <FormControl>
+//                                             <Input
+//                                                 placeholder="shadcn"
+//                                                 {...field}
+//                                             />
+//                                         </FormControl>
+//                                         <FormDescription>
+//                                             This is your public display name.
+//                                         </FormDescription>
+//                                         <FormMessage />
+//                                     </FormItem>
+//                                 )}
+//                             />
+//                             <Button type="submit">Submit</Button>
+
+//                             <Collapsible>
+//                                 <CollapsibleTrigger>
+//                                     Perfil demográfico
+//                                 </CollapsibleTrigger>
+//                                 <CollapsibleContent>
+//                                     Yes. Free to use for personal and commercial
+//                                     projects. No attribution required.
+//                                 </CollapsibleContent>
+//                             </Collapsible>
+//                             <Collapsible>
+//                                 <CollapsibleTrigger>
+//                                     Configuración de tono del bot
+//                                 </CollapsibleTrigger>
+//                                 <CollapsibleContent>
+//                                     Yes. Free to use for personal and commercial
+//                                     projects. No attribution required.
+//                                 </CollapsibleContent>
+//                             </Collapsible>
+//                         </Form>
+//                     </DialogDescription>
+//                 </DialogHeader>
+//             </DialogContent>
+//         </Dialog>
+//     );
+// };
+
+// export default SecondaryDashboard;
