863b9f564f63f5d9d11926b6d6a508002b95d10c
Joel Alejandro Milla Lopez <jalejandro.milla@outlook.com>
2024-09-15 07:17:14 -0600

    Improve title of page.

--- dashboard/src/MainDashboard/Title.tsx
+++ dashboard/src/MainDashboard/Title.tsx
@@ -3,17 +3,17 @@ import { Button } from "@nextui-org/react";
 
 export default function Title() {
   return (
-    <div className="flex items-center justify-between mb-6">
+    <div className="flex items-center justify-between mb-4">
       <div>
-        <h1 className="text-3xl font-bold text-gray-800 dark:text-gray-100">
-          Usuarios (10)
+        <h1 className="text-3xl font-bold text-gray-800 dark:text-gray-100 pb-1">
+          Campañas Activas
         </h1>
         <p className="text-gray-500 dark:text-gray-400">
-          Administra usuarios (funcionalidades de tabla en el lado del cliente).
+          Administra las campañas (creación, edición y seguimiento).
         </p>
       </div>
       <Button size="md" color="secondary" className="dark:bg-purple-700">
-        + Añadir Nuevo
+        + Crear Nueva Campaña
       </Button>
     </div>
   );
